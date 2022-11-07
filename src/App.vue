a4f64bdeb3e4c0a3df25a9a7db5e293596c3617b
Prico3 <pricoco.andrea@gmail.com>
2022-11-07 17:06:30 +0100

    bonus completed

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,36 @@
 <script>
+import AppBannerVue from './components/AppBanner.vue';
+
 export default {
-  data(){
-    return{
+  data() {
+    return {
 
     }
   },
+  components: {
+    AppBannerVue
+  }
 }
 </script>
 
 <template>
-  <h1>Questa è la mia prima app con <em>Vite</em>  + <em>Vue</em> </h1>
+  <h1>Questa è la mia prima app con <em>Vite</em> + <em>Vue</em> </h1>
+  <AppBannerVue />
 </template>
 
 <style>
-body{
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
   background-color: black;
 }
-h1{
+
+h1 {
+  text-align: center;
   color: aquamarine;
 }
 </style>
---
+++ src/components/AppBanner.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="banner">
+        <h4>Questo è il mio primo banner</h4>
+    </div>
+</template>
+
+<style>
+.banner {
+    background-color: blanchedalmond;
+    width: 100%;
+    height: 400px;
+    padding: 2rem;
+    margin-top: 2rem;
+    border-radius: 20px;
+}
+
+h4 {
+    color: black;
+    text-align: center;
+
+}
+</style>
